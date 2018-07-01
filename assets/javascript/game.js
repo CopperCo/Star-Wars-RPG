b13790c3885923ed10bb5a4e2a05e1ddbdf7af11
Hayley Sweet <mrs.kornilenko@gmail.com>
2018-07-01 11:47:59 -0400

    game dev progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,6 +37,32 @@ function loadCharacter(character) {
         "</p></div>"
     );
   }
+  if (character.team === "enemies") {
+    $(".enemies").append(
+      "<div class='col-md-3 enemy' id='" +
+        character.name +
+        "'><p> " +
+        character.name +
+        "</p><img src=" +
+        character.image +
+        "><p>" +
+        character.hp +
+        "</p></div>"
+    );
+  }
+  if (character.team === "defender") {
+    $(".defender-holder").append(
+      "<div class='col-md-3 defender' id='" +
+        character.name +
+        "'><p> " +
+        character.name +
+        "</p><img src=" +
+        character.image +
+        "><p>" +
+        character.hp +
+        "</p></div>"
+    );
+  }
 }
 
 var characters = {
@@ -53,21 +79,65 @@ var characters = {
 };
 
 $(document).ready(function() {
-  $(".available-characters").html("<div></div>");
-  $(".your-character").html("<div></div>");
   for (name in characters) {
     loadCharacter(characters[name]);
   }
 
-  $(".character").on("click", function() {
-    character = characters[this.id];
-    if (character.team === null) {
-      character.team = "my";
-    }
-    $(".available-characters").html("<div></div>");
-    $(".your-character").html("<div></div>");
+  // $(".character").on("click", function() {
+  //   character = characters[this.id];
+  //   character.team = "my";
+  //   $(".available-characters").html("<div></div>");
+  //   for (name in characters) {
+  //     console.log(characters[name]);
+  //     if (characters[name].team != "my") {
+  //       characters[name].team = "enemies";
+  //     }
+  //     loadCharacter(characters[name]);
+  //   }
+  //   $(".enemy").on("click", function() {
+  //     console.log(this.id);
+  //     characters[this.id].team = "defender";
+  //     loadCharacter(characters[this.id]);
+  //   });
+  // });
+
+  function selectDefender() {
+    characters[this.id].team = "defender";
+    $("#" + this.id)
+      .detach()
+      .appendTo(".defender-holder")
+      .attr("class", "col-md-3 defender");
+  }
+
+  function selectCharacter() {
+    characters[this.id].team = "my";
+    $("#" + this.id)
+      .detach()
+      .unbind()
+      .appendTo(".your-character")
+      .attr("class", "col-md-3 fighter");
     for (name in characters) {
-      loadCharacter(characters[name]);
+      console.log(name);
+      if (characters[name].team != "my") {
+        characters[name].team = "enemies";
+        $("#" + name)
+          .detach()
+          .appendTo(".enemies")
+          .attr("class", "col-md-3 enemy")
+          .unbind()
+          .click(selectDefender);
+      }
     }
+  }
+
+  function fight() {
+    characters[name].attackScore = attackScore * 2;
+  }
+
+  $(".character").on("click", selectCharacter);
+
+  $("#attackBtn").on("click", function() {
+    alert("You attacked");
+    fight();
   });
 });
